10dbdbb4f92dce03b7268eae3adce18bafe017f9
Tomasz Wylezek <tomwylezek@gmail.com>
2020-04-04 11:36:11 +0200

    Styles

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,7 +30,7 @@ export default {
   /*
    ** Global CSS
    */
-  css: [],
+  css: ['@/assets/scss/main.scss', 'github-markdown-css'],
   /*
    ** Plugins to load before mounting the App
    */
@@ -61,12 +61,15 @@ export default {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {
+    extend(config, _ctx) {
       config.module.rules.push({
         test: /\.md$/,
         loader: 'frontmatter-markdown-loader',
         options: {
           mode: [FMMode.VUE_COMPONENT],
+          vue: {
+            root: 'markdown-body',
+          },
         },
       });
     },
